d2374b98adb1b1880ff73e06d4c6071045035dde
ssutop <ssutopeye@gmail.com>
2022-03-03 23:42:19 +0900

    언제까지 어깨춤?

--- src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ProfileController {
     private final Environment env;
 
+//    체크
     @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
